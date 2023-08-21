911159e37f9234cdc1e929c86c62f387ece54e49
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-08-21 17:13:33 -0400

    Update snl_stream.py

--- snl_stream.py
+++ snl_stream.py
@@ -4,16 +4,17 @@ import random
 import subprocess
 import threading
 from time import sleep
+import re
 
 video_queue = queue.Queue()
 urls_file = 'urls.txt'
 
 def download_video(url):
     video_title = subprocess.check_output(['yt-dlp', '-f', 'best[ext=mp4]+bestaudio[ext=m4a]/mp4', '--get-title', url]).decode().strip()
-    sanitized_video_title = video_title.replace(':', '_')  # Replace colons with underscores
+    sanitized_video_title = re.sub(r'\W+', '', video_title)
     video_file = f'{sanitized_video_title}.mp4'
     subprocess.run(['yt-dlp', '-f', 'best[ext=mp4]+bestaudio[ext=m4a]/mp4', '-o', video_file, url])
-    return f"C:\\Users\\totob\\Downloads\\snl\\{video_file}"
+    return f"{video_file}"
 
 def enqueue_video(video_path):
     video_queue.put(video_path)
@@ -71,17 +72,17 @@ def broadcast_video(video_path, stream_url, stream_key):
 
     try:
         subprocess.run(ffmpeg_cmd, check=True)
-        os.remove(video_path)  # delete the video file after broadcasting
+        os.remove(video_path) 
         print(f"Finished broadcasting and deleted video file: {video_path}")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while broadcasting the video: {e}")
     except KeyboardInterrupt:
-        os.remove(video_path)  # Attempt to delete video file in case of interruption
+        os.remove(video_path)
         print("Broadcast interrupted by user.")
 
 def start_broadcast(stream_url, stream_key):
     while True:
-        while video_queue.qsize() < 3 and urls:  # Download videos until there are at least 3 in the queue
+        while video_queue.qsize() < 3 and urls:
             url = urls.pop()
             try:
                 video_file = download_video(url)
@@ -94,26 +95,21 @@ def start_broadcast(stream_url, stream_key):
             print(f"Now broadcasting: {video_path}")
             broadcast_video(video_path, stream_url, stream_key)
         elif not urls:
-            break  # Break the loop if all videos have been broadcasted and there are no more URLs to download
+            break
         else:
-            loop_video_path = 'C:\\Users\\totob\\Downloads\\snl\\wait.mp4'  # Specify the loop video path
+            loop_video_path = 'wait.mp4'
             print(f"Now looping: {loop_video_path}")
             broadcast_video(loop_video_path, stream_url, stream_key)
 
-# Main script execution
-# stream_url = 'rtmp://a.rtmp.youtube.com/live2'  # youtube
-# stream_key = 'sx5j-wds6-77k5-pwsx-8khy'  # youtube
-stream_url = 'rtmp://localhost:1935'  # api.video
-stream_key = '05ff367e-d348-4b52-99a7-e54150ce8263'  # api.video
+stream_url = 'rtmp://localhost:1935'
+stream_key = 'haha'
 
 with open(urls_file, 'r') as file:
     urls = file.readlines()
-random.shuffle(urls)  # Shuffle URLs before starting
+random.shuffle(urls)
 
-# Start the broadcasting loop
 start_broadcast(stream_url, stream_key)
 
-# Shuffle URLs and start broadcasting loop again
 while True:
     print('Finished broadcasting all videos. Starting over...')
     random.shuffle(urls)
